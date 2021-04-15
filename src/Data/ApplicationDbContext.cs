90a57cd0aa90e75a84814b2aefcc404923d78470
carvalho57 <gabrielfeocarvalho57@gmail.com>
2021-04-14 21:21:53 -0300

    Alterado o modo de registrar os mapeamentos das entidades

--- src/Data/ApplicationDbContext.cs
+++ src/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using CasaCodigo.Data.Mapping;
 using CasaCodigo.Entities;
 using Flunt.Notifications;
@@ -12,10 +13,11 @@ namespace CasaCodigo.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);             
-            modelBuilder.Ignore<Notification>();            
-            modelBuilder.ApplyConfiguration(new AuthorMapping());
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Ignore<Notification>();
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());            
         }
-            public DbSet<Author> Authors { get; private set; }
+        public DbSet<Author> Authors { get; private set; }
+        public DbSet<Category> Categories { get; private set; }
     }
 }
