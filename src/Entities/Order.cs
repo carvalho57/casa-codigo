5ef99a1e09747028405315de77f95cb365d2c69c
carvalho57 <gabrielfeocarvalho57@gmail.com>
2021-04-24 18:25:41 -0300

    Adicionando subtotal para entidade order

--- src/Entities/Order.cs
+++ src/Entities/Order.cs
@@ -13,26 +13,34 @@ namespace CasaCodigo.Entities
         {
 
             AddNotifications(new Contract().Requires().IfNotNull(customer, order => order.Join(customer)));
-            Customer = customer;            
+            Customer = customer;
         }
         private List<OrderItem> _items { get; set; } = new List<OrderItem>();
         public IEnumerable<OrderItem> Items => _items.AsReadOnly();
         public Customer Customer { get; private set; }
         public decimal Discount { get; private set; }
+        public decimal SubTotal
+        {
+            get
+            {
+                return _items.Sum(item => item.Total());
+            }
+        }
         public decimal Total
         {
             get
             {
-                var total =  _items.Sum(item => item.Total());
-                return total - (total * (Discount/100));
+                
+                return SubTotal - (SubTotal * (Discount / 100));
             }
         }
-        public void ApplyCoupon(Coupon coupon){            
+        public void ApplyCoupon(Coupon coupon)
+        {
             AddNotifications(new Contract().Requires()
                 .IsNotNull(coupon, nameof(Discount), "O cupom está inválido")
                 .IsTrue(coupon.IsValid(), nameof(Discount), "O cupom está expirado")
             );
-            if(Valid)
+            if (Valid)
                 Discount = (decimal)coupon.Percentage;
         }
         public void AddItem(OrderItem item)
